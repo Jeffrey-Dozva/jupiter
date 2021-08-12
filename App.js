f8b56d326565bdbd2be673c42c3abbff4e6ed085
Jeffrey Dozva <jeffreydozva@gmail.com>
2021-08-12 14:29:16 +0200

    GPay test

--- App.js
+++ App.js
@@ -31,38 +31,27 @@ import Judo, {
   JudoAuthorization,
   JudoTransactionType,
   JudoTransactionMode,
-  // JudoGooglePayConfiguration,     
-JudoGooglePayEnvironment,     
-JudoAddressFormat,     
-JudoBillingAddressParameters,     
-JudoShippingAddressParameters 
+  JudoGooglePayConfiguration,
+JudoGooglePayEnvironment,
+JudoAddressFormat,
+  JudoCardNetwork,
+JudoBillingAddressParameters,
+JudoShippingAddressParameters
 } from 'judokit-react-native'
+import {JudoUIConfiguration} from "judokit-react-native/types/JudoTypes";
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+// enum JudoCardNetwork {
+//   Visa = 1 << 0,
+//     Mastercard = 1 << 1,
+//     Maestro = 1 << 2,
+//     Amex = 1 << 3,
+//     ChinaUnionPay = 1 << 4,
+//     JCB = 1 << 5,
+//     Discover = 1 << 6,
+//     DinersClub = 1 << 7,
+//     All = 1 << 8,
+// }
+
 
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -70,118 +59,82 @@ const App: () => Node = () => {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
-const executePayment =   ()=>{
-  console.log('pressed the button')
+  const executePayment =   ()=>{
+    console.log('pressed the button');
+    const cardNetworks = JudoCardNetwork.Visa | JudoCardNetwork.Mastercard
 
-
-  var JudoBillingAddressParameters = {     
-    addressFormat: JudoAddressFormat.MINIMAL,     
-    isPhoneNumberRequired: true, 
-    } 
-     
-    var JudoShippingAddressParameters = {    
-    allowedCountryCodes: ["GB", "US"],     
-    isPhoneNumberRequired: false, 
-    } 
-     
-   var JudoGooglePayConfiguration = {     
-    countryCode: 'GB',     
-    environment: JudoGooglePayEnvironment.TEST,     
-    isEmailRequired: true,     
-    isBillingAddressRequired: true,     
-    billingAddressParameters: JudoBillingAddressParameters,     
-    isShippingAddressRequired: true,     
-    shippingAddressParameters: JudoShippingAddressParameters 
+    var JudoBillingAddressParameters = {
+      addressFormat: JudoAddressFormat.MINIMAL,
+      isPhoneNumberRequired: true,
     }
-
-  var auth = { token: '', secret: '' };
-  var judo = new Judo(auth);
-  // 2. Set the Judo session to sandbox mode for testing
-  judo.isSandboxed = true;
-  // 3. Create a Judo configuration to setup your payment flow
-  var amount = {
+    var JudoShippingAddressParameters = {
+      allowedCountryCodes: ["GB", "US"],
+      isPhoneNumberRequired: false,
+    };
+    var JudoGooglePayConfiguration = {
+      countryCode: 'GB',
+      environment: JudoGooglePayEnvironment.TEST,
+      isEmailRequired: true,
+      isBillingAddressRequired: true,
+      billingAddressParameters: JudoBillingAddressParameters,
+      isShippingAddressRequired: true,
+      shippingAddressParameters: JudoShippingAddressParameters
+    }
+    var auth = {
+      token: '',
+      secret:
+        '',
+    };
+    var judo = new Judo(auth);
+    judo.isSandboxed = true;
+    var amount = {
       value: '0.01',
       currency: 'GBP',
-  };
-  var reference = {
-      consumerReference: 'MY-CONSUMER-REFERENCE',
-      paymentReference: 'MY-PAYMENT-REFERENCE'
-  };
-  var configuration = {
-      judoId: '100177237',
+    };
+    var reference = {
+      consumerReference: 'MY-CONSUMER-REFERENCE-1',
+      paymentReference: 'MY-PAYMENT-REFERENCE-1'
+    };
+    var configuration = {
+      judoId: '',
       countryCode: 'GB',
       amount,
       reference,
-      googlePayConfiguration: JudoGooglePayConfiguration
+      uiConfiguration: {
+        isAVSEnabled: true,
+        shouldPaymentMethodsDisplayAmount: true,
+        shouldPaymentButtonDisplayAmount: true,
+        shouldPaymentMethodsVerifySecurityCode: true,
+      },
+      supportedCardNetworks: cardNetworks,
+      googlePayConfiguration: JudoGooglePayConfiguration,
+      isInitialRecurringPayment: false,
   };
 
-  // judo.invokeApplePay(2, configuration)
-  // .then((response) => {/* Handle response */})
-
-  // return
-
-  // judo.invokeGooglePay()
-  judo.invokeGooglePay(JudoTransactionMode.ServerToServer, configuration)
-  .then((response) => {
-    /* Handle response */
-  console.log("respose from  invokePaymentMethodScreen - ", response)
-  })
-  .catch((error) => {
-    /* Handle error */
-    console.log("respose from  invokePaymentMethodScreen CATCH - ", error)
-  
-  })
-
-  return
-
-  // 4. Invoke a payment transaction and handle the result
-
-  // judo.invokeGooglePay(JudoTransactionMode.Payment, configuration)
-  // .then((response) => {/* Handle response */})
-
-
-
   judo.invokeGooglePay(JudoTransactionMode.Payment, configuration)
-      .then(function (response) {
-        console.log('here is the response for successfull', response)
-      // Handle response
+      .then( (response) => {
+        console.log({response})
+
+  }).catch((error) => {
+    console.log({error})
+
   })
-  //     .catch(function (error) {
-  //       console.log('Opppps man you have an errodjss', error)
-  //     // Handle error
-  // });
+
 
 
   }
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      
+
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
+
             <Button title="Press me" onPress= {executePayment}> press here </Button>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+
+
       </ScrollView>
     </SafeAreaView>
   );
